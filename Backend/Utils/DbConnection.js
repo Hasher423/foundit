cabd741ca318723eac7b5d022b4463fbcdfc8bdd
HASHER <hasher423@gmail.com>
2025-12-23 10:52:33 +0500

    Postmessage setted

--- Backend/Utils/DbConnection.js
+++ Backend/Utils/DbConnection.js
@@ -1,14 +1,11 @@
 import mongoose from "mongoose";
 
-let isConnected = false;
 
 const connectDB = async () => {
   try {
-    if (isConnected) return;
     const conn = await mongoose.connect(process.env.DB_URL);
     console.log(`MongoDB Connected`);
 
-    isConnected = true;
   } catch (error) {
     console.error(`Error: ${error.message}`);
   }
